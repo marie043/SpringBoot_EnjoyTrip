fdf0f36a4d6098ebd0f447b7b501378ef944d20e
taedong <etd93@naver.com>
2023-11-07 06:04:50 +0900

    dto 구현

---
+++ EnjoyTrip/src/main/java/com/ssafy/enjoy/member/controller/MemberController.java
@@ -0,0 +1,32 @@
+package com.ssafy.enjoy.member.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class MemberController {
+	@GetMapping("/login")
+	public Map<String, String> Member(@RequestBody String id, HttpSession session){
+		Map<String, String> result = new HashMap<String, String>();
+		if(id==null) {
+			result.put("msg", "NO");
+			result.put("detail", "no input id");
+		}else {
+			if(id.equals(session.getAttribute("id"))) {
+				result.put("msg", "OK");
+			}else {
+				result.put("msg", "NO");
+				result.put("detail", "no matching id");
+			}
+		}
+		return result;
+	}
+}
