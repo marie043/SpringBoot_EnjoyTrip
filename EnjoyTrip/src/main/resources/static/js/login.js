6c3ae977a7ae1f474efdbe2833cf5962eadc78f1
KimSiEun <sieun043@gmail.com>
2023-11-07 16:43:41 +0900

    close modal 구현

--- EnjoyTrip/src/main/resources/static/js/login.js
+++ EnjoyTrip/src/main/resources/static/js/login.js
@@ -1,10 +1,20 @@
+document.querySelectorAll('.close-area').forEach(element=>{
+	element.addEventListener('click', closeModal);
+});
+
 document.getElementById('login-button').addEventListener('click', function(){
-	console.log('로그인 버튼 눌림');
+	
 });
 
 document.getElementById('login-a').addEventListener('click', function(){
 	document.getElementById('login-modal').style.display='flex';
 });
+
 document.getElementById('join-a').addEventListener('click', function(){
 	document.getElementById('join-modal').style.display='flex';
 });
+
+function closeModal(){
+	document.getElementById('login-modal').style.display='none';
+	document.getElementById('join-modal').style.display='none';
+}
