09fb97fd83a0137b189624fbcaa328517caf3e30
KimSiEun <sieun043@gmail.com>
2023-11-09 11:25:39 +0900

    please....

--- EnjoyTrip/src/main/resources/static/js/map.js
+++ EnjoyTrip/src/main/resources/static/js/map.js
@@ -3,7 +3,7 @@ function changeGugun(){
 	const select = document.getElementById('search-area');
 	const sido = select[select.selectedIndex].value;
 	console.log(sido);
-	document.querySelectorAll('#search-sigungu>option').forEach(function(e){
+	document.querySelectorAll('#search-sigungu option').forEach(function(e){
 		if(e.getAttribute('class')==sido){
 			e.style.display = 'block';
 		}else{
