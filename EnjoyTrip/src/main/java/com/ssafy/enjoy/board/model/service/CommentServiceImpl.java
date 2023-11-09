6ca014130c2a0a879c43546486901034fc5dbf02
KimSiEun <sieun043@gmail.com>
2023-11-09 17:32:43 +0900

    annotation 추가

--- EnjoyTrip/src/main/java/com/ssafy/enjoy/board/model/service/CommentServiceImpl.java
+++ EnjoyTrip/src/main/java/com/ssafy/enjoy/board/model/service/CommentServiceImpl.java
@@ -3,11 +3,13 @@ package com.ssafy.enjoy.board.model.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ssafy.enjoy.board.model.Board;
 import com.ssafy.enjoy.board.model.Comment;
 import com.ssafy.enjoy.board.model.mapper.CommentMapper;
 
+@Service
 public class CommentServiceImpl implements CommentService{
 
 	@Autowired
