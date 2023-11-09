5035ac8b4bc75871c773ae69ed1229b7297bd15d
marie043 <sieun043@gmail.com>
2023-11-09 20:18:08 +0900

    comment service, mapper 구현

--- EnjoyTrip/src/main/java/com/ssafy/enjoy/board/model/service/CommentServiceImpl.java
+++ EnjoyTrip/src/main/java/com/ssafy/enjoy/board/model/service/CommentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.enjoy.board.model.service;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,21 +11,29 @@ import com.ssafy.enjoy.board.model.Comment;
 import com.ssafy.enjoy.board.model.mapper.CommentMapper;
 
 @Service
-public class CommentServiceImpl implements CommentService{
+public class CommentServiceImpl implements CommentService {
 
 	@Autowired
 	CommentMapper commentMapper;
-	
+
 	@Override
 	public List<Comment> getList(Board board) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			return commentMapper.readComment(board);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new Exception("Server error");
+		}
 	}
 
 	@Override
 	public void writeComment(Comment comment) throws Exception {
-		// TODO Auto-generated method stub
-		
+		try {
+			commentMapper.creatComment(comment);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new Exception("Server error");
+		}
 	}
 
 }
